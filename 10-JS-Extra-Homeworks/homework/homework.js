4e759f874044f876e79b03160394ffc64aca8a14
carlos <carlosamado221471@gmail.com>
2022-06-02 13:56:33 -0300

    hw10terminado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,6 +1,6 @@
 // No cambies los nombres de las funciones.
 
-function deObjetoAmatriz(objeto){
+function deObjetoAmatriz(objeto) {
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
   //Ejemplo: 
@@ -10,7 +10,14 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var nuevoarray = [];
+  for (const clave in objeto) {
+    nuevoarray.push([clave, objeto[clave]])
+  }
+  return nuevoarray
 }
+
+
 
 
 function numberOfCharacters(string) {
@@ -18,7 +25,21 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var letras = {};
+  for (let i = 0; i < string.length; i++) {
+    if (letras.hasOwnProperty(string[i])) {
+      letras[string[i]] += 1
+    } else {
+      letras[string[i]] = 1
+    }
+  }
+  return letras;
 }
+
+
+
+
+
 
 
 function capToFront(s) {
@@ -26,6 +47,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var minusculas = "";
+  var mayusculas = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].toUpperCase() === s[i]) {
+      mayusculas += s[i]
+    } else {
+      minusculas += s[i]
+    }
+  }
+  return mayusculas + minusculas;
 }
 
 
@@ -35,21 +66,43 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  var invertir = [];
+  for (let i = 0; i < str.length; i++) {
+    invertir.unshift(str[i]);
+    
+  }
+  return invertir.join('').split(' ').reverse().join(' ')
+
+}
 
 
-function capicua(numero){
+function capicua(numero) {
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var invertido =numero.toString();
+  if(invertido.split('').reverse().join('')== numero){
+    return "Es capicua";
+  }else{
+    return "No es capicua"; 
+  }
 }
 
 
-function deleteAbc(cadena){
+function deleteAbc(cadena) {
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var nuevacadena = "";
+  for (let i = 0; i < cadena.length; i++) {
+  if(cadena[i]!== "a"&&cadena[i]!== "b" && cadena[i]!== "c"){
+    nuevacadena += cadena[i]
+  }else{
+    continue;
+  }    
+  }
+  return nuevacadena;
 }
 
 
@@ -57,15 +110,26 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  return arr.sort((a,b) => a.length - b.length)
 }
 
 
-function buscoInterseccion(arreglo1, arreglo2){
+function buscoInterseccion(arreglo1, arreglo2) {
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var nuevoarray =[];
+  for (let i = 0; i < arreglo2.length; i++) {
+       for (let j = 0; j < arreglo2.length; j++) {
+         if(arreglo1[i]===arreglo2[j]){
+           nuevoarray.push(arreglo1[i])
+         }
+         
+       } 
+  } 
+  return nuevoarray;  
 }
 
 
@@ -74,13 +138,13 @@ function buscoInterseccion(arreglo1, arreglo2){
 // --------------------------------
 
 module.exports = {
-   deObjetoAmatriz,
-   numberOfCharacters,
-   capToFront,
-   asAmirror,
-   capicua,
-   deleteAbc,
-   sortArray,
-   buscoInterseccion,
+  deObjetoAmatriz,
+  numberOfCharacters,
+  capToFront,
+  asAmirror,
+  capicua,
+  deleteAbc,
+  sortArray,
+  buscoInterseccion,
 };
 
